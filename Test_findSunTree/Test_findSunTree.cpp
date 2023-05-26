2bf2ddaa368dbc8171e895e5e7e1f9df9ddf759f
Кулаков Никита <kulakofnikita@gmail.com>
2023-05-26 14:40:05 +0300

    Исправлены ошибки в указании адресов каталогов у некоторых тестов

---
+++ Test_findSunTree/Test_findSunTree.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestfindSunTree
+{
+	TEST_CLASS(TestfindSunTree)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
